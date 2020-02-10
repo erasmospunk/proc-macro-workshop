509f3b79d2f8d722a93047b6552a47eac419cb54
John L. Jegutanis <erasmospunk@gmail.com>
2020-02-10 22:54:34 +0200

    Use absolute paths

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -16,7 +16,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let cmd = &input.ident;
     let cmd_builder = format_ident!("{}Builder", cmd);
 
-    let fields: Vec<(Field, bool, Option<Ident>)> = if let Data::Struct(
+    let fields: Vec<(Field, bool, core::option::Option<Ident>)> = if let Data::Struct(
         DataStruct{
             struct_token: _,
             fields: Fields::Named(
@@ -40,7 +40,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             };
 
             // Find if attributes are present
-            let mut attribute: Option<Ident> = None;
+            let mut attribute: core::option::Option<Ident> = core::option::Option::None;
             for attr in &field.attrs {
                 if let Ok(Meta::List(meta)) = attr.parse_meta() {
                     if !meta.path.is_ident("builder") { continue; }
@@ -48,7 +48,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
                         if let NestedMeta::Meta(Meta::NameValue(m)) = &meta.nested[0] {
                             if m.path.is_ident("each") {
                                 if let Lit::Str(name) = &m.lit {
-                                    attribute = Some(format_ident!("{}", name.value()));
+                                    attribute = core::option::Option::Some(format_ident!("{}", name.value()));
                                 }
                             } else {
                                 return syn::Error::new_spanned(
@@ -64,7 +64,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             let mut field: Field = field.clone();
             // For non optional fields, wrap them in Option
             if !optional {
-                field.ty = parse_quote! {Option<#ty>};
+                field.ty = parse_quote! {core::option::Option<#ty>};
             }
             // Clear attributes
             field.attrs.clear();
@@ -85,10 +85,10 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let fields_init_none = fields.iter().map(|(f, _, each)| {
         let i = f.ident.as_ref().unwrap();
         if each.is_none() {
-            quote_spanned!(i.span() => #i: None)
+            quote_spanned!(i.span() => #i: core::option::Option::None)
         } else {
             // When dealing with `each` variants assume a vec
-            quote_spanned!(i.span() => #i: Some(Vec::new()))
+            quote_spanned!(i.span() => #i: core::option::Option::Some(Vec::new()))
         }
     });
 
@@ -97,25 +97,25 @@ pub fn derive(input: TokenStream) -> TokenStream {
         // Extract T from Option<T>
         let t = extract_inner_type(&f.ty, "Option".into());
 
-        let one_at_a_time_method = if let Some(new_i) = each {
+        let one_at_a_time_method = if let core::option::Option::Some(new_i) = each {
             if let GenericArgument::Type(t) = &t[0] {
                 // Extract T from Vec<T>
                 let new_t = extract_inner_type(t, "Vec".into());
-                Some(quote_spanned!(i.span() => fn #new_i(&mut self, #new_i: #new_t) -> &mut Self {
+                core::option::Option::Some(quote_spanned!(i.span() => fn #new_i(&mut self, #new_i: #new_t) -> &mut Self {
                     if self.#i.is_none() {
-                        self.#i = Some(Vec::new());
+                        self.#i = core::option::Option::Some(Vec::new());
                     }
                     self.#i.as_mut().unwrap().push(#new_i);
                     self
                 }))
-            } else { None }
-        } else { None };
+            } else { core::option::Option::None }
+        } else { core::option::Option::None };
 
         // Generate the normal methods if no `each` attribute is provided OR if the attribute name
         // differs from the structs' member name.
         let all_at_once_method = if each.is_none() || each.as_ref().unwrap() != i {
             quote_spanned!(i.span() => fn #i(&mut self, #i: #t) -> &mut Self {
-                self.#i = Some(#i);
+                self.#i = core::option::Option::Some(#i);
                 self
             })
         } else { quote! {} };
@@ -147,7 +147,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #cmd_builder {
             #(#cmd_builder_setters)*
 
-            pub fn build(&mut self) -> Result<#cmd, Box<dyn Error>> {
+            pub fn build(&mut self) -> core::result::Result<#cmd, std::boxed::Box<dyn Error>> {
                 Ok(#cmd {
                     #(#build_commands),*
                 })
@@ -169,13 +169,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn extract_inner_type(ty: &Type, outer_id: String) -> &Punctuated<GenericArgument, Comma> {
     match ty {
         Type::Path(p) => {
-            assert_eq!(p.path.segments[0].ident, outer_id);
-            match &p.path.segments[0].arguments {
-                PathArguments::AngleBracketed(a) => {
-                    &a.args
-                },
-                _ => unimplemented!(),
+            for segment in &p.path.segments {
+                if segment.ident != outer_id { continue }
+                if let PathArguments::AngleBracketed(a) = &segment.arguments {
+                    return &a.args;
+                }
             }
+            panic!("Unexpected format");
         },
         _ => unimplemented!(),
     }
